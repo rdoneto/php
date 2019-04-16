d1d9a4b7ad65f403f817e079b7661f10bcc1043c
rdoneto <rdoneto_@hotmail.com>
2019-04-16 19:35:14 -0300

    resolvi REALMENTE o conflito

--- index.php
+++ index.php
@@ -1,14 +1,9 @@
 <?php
 
-<<<<<<< HEAD
   echo "Olá Mundo 4 5 6!";
   
-  echo "Olá Udemy!";
-=======
-  echo "Olá Mundo 1 2 3!";
+  echo "Olá Udemy e HCode!";
 
-  echo "Olá HCode!";
->>>>>>> b1036aecefe35c5057d6bb7695f017fc5b06212c
 
 
 ?>
